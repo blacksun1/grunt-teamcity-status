27b333494d6ef553c872489e227c00cf31f9c5eb
Simon Bruce <Simon.Bruce@rbi.co.uk>
2016-05-03 14:40:31 +0100

    Changed reporting on Tests to verbose.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,37 +6,40 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
+"use strict";
 
 module.exports = function (grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
+    "jshint": {
+      "all": [
+        "Gruntfile.js",
+        "tasks/*.js",
+        "<%= nodeunit.tests %>"
       ],
-      options: {
-        jshintrc: '.jshintrc'
+      "options": {
+        "jshintrc": ".jshintrc"
       }
     },
 
     // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
+    "nodeunit": {
+      "options": {
+        "reporter": "default"
+      },
+      "tests": ["test/*_test.js"]
     }
   });
 
   // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+  grunt.loadTasks("tasks");
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-nodeunit");
 
-  grunt.registerTask('default', ['teamcity_status']);
-  grunt.registerTask('test', ['nodeunit']);
+  grunt.registerTask("default", ["teamcity_status"]);
+  grunt.registerTask("test", ["nodeunit"]);
 };
